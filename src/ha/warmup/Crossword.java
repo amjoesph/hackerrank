a71880621b6f25bf877443cafb784a95f7c596c5
amjoesph <manthoni@p23088.ieee.org>
2017-06-06 10:23:27 -0400

    Passed all test

--- src/ha/warmup/Crossword.java
+++ src/ha/warmup/Crossword.java
@@ -2,7 +2,10 @@ package ha.warmup;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Crossword {
@@ -64,9 +67,7 @@ public class Crossword {
 				isfound = false;
 				k++;
 			} 
-//			else {
-//				rows[k]="";
-//			}
+
 			for (int j = 0; j < 10; j++) {
 				if (matrix[i][j].equals(char2Check) && 
 						( (j > 0 && matrix[i][j-1].equals(char2Check)) || (j+1 < 10 && matrix[i][j+1].equals(char2Check))) ) {
@@ -77,39 +78,6 @@ public class Crossword {
 					isfound = true;
 				}
 			}
-			
-//			if (rows[k] == null || rows[k] == "") {
-//				continue;
-//			}
-//			String[] positions = rows[k].split(";");
-//			if (positions.length > 1) {
-//				// TODO: to filter
-//				int startPos = Integer.parseInt(positions[0].split(",")[1]);
-//				int wlen = 1;
-//				for (int l=1; l< positions.length; l++){
-//					int endPos = Integer.parseInt(positions[l].split(",")[1]);
-//					if (startPos+1 != endPos) {
-//						if (wlen > 1 && matchWordsLen(wlen)) {
-//							isfound = true;
-//						}
-//					} else {
-//						startPos = endPos;
-//						wlen++;
-//					}
-//				}
-//				
-//				if (wlen > 1 && matchWordsLen(wlen)) {
-//					isfound = true;
-//				} else {
-//					isfound = false;
-//					rows[k]="";
-//				}
-//				
-//			} else {
-//				isfound = false;
-//				rows[k]="";
-//			}
-			
 		}		
 		return rows;
 	}
@@ -134,40 +102,6 @@ public class Crossword {
 					isfound = true;
 				}
 			}
-			
-//			if (rows[k] == null || rows[k] == "") {
-//				continue;
-//			}
-			
-//			String[] positions = rows[k].split(";");
-//			if (positions.length > 1) {
-//				// TODO: to filter
-//				int startPos = Integer.parseInt(positions[0].split(",")[0]);
-//				int wlen = 1;
-//				for (int l=1; l< positions.length; l++){
-//					int endPos = Integer.parseInt(positions[l].split(",")[0]);
-//					if (startPos+1 != endPos) {
-//						if (wlen > 1 && matchWordsLen(wlen)) {
-//							isfound = true;
-//						}
-//					} else {
-//						startPos = endPos;
-//						wlen++;
-//					}
-//				}
-//				
-//				if (wlen > 1 && matchWordsLen(wlen)) {
-//					isfound = true;
-//				} else {
-//					isfound = false;
-//					rows[k]="";
-//				}
-//				
-//			} else {
-//				isfound = false;
-//				rows[k]="";
-//			}
-			
 		}		
 		return rows;
 	}
@@ -228,9 +162,91 @@ public class Crossword {
 			String toFill = "";
 			
 			for (String in : input) {
+//				System.out.println("in string:" + in);
 				if (in.length() == len && !inputUsed.contains(in)) {
-					toFill = in;
-					break;
+//					System.out.println("fillll:" + row);
+					Map<String,String> positionalIndexes = new HashMap<String, String>();
+					Map<String,String> interPositionIndex = new HashMap<String, String>();
+					
+					for (String inter : interScan) {
+						if (inter != null && row.indexOf(inter) != -1) {
+							// to get a map of intersected position and letters 
+							// e.g. 3,1; is D
+							String letter = String.valueOf(in.charAt(row.indexOf(inter)/4));
+							positionalIndexes.put(inter, letter);
+							
+							// to get a map of position and letters 
+							// e.g. 3 is D
+							for (String col : colScan) {
+								if (col != null && col.indexOf(inter) != -1) {
+									interPositionIndex.put(String.valueOf(col.indexOf(inter)/4), letter);
+								}
+							}
+							
+						}
+					}
+					
+					
+					if (positionalIndexes.size() > 0) {
+						Iterator<String> itr = positionalIndexes.keySet().iterator();
+						while(itr.hasNext()) {
+							String key = itr.next();
+//							System.out.println(":position index:" + key + ":va:" + positionalIndexes.get(key));
+						}
+						
+						itr = interPositionIndex.keySet().iterator();
+						while(itr.hasNext()) {
+							String key = itr.next();
+//							System.out.println(":intersection position index:" + key + ":iiiva:" + interPositionIndex.get(key));
+						}
+						
+						itr = interPositionIndex.keySet().iterator();
+						boolean inStrPositionContains = false;
+						Boolean[] positionalStatus = new Boolean[interPositionIndex.keySet().size()];
+						Arrays.fill(positionalStatus, Boolean.FALSE);
+						int pos = 0;
+						while(itr.hasNext()) {
+							String key = itr.next();
+							String _char = interPositionIndex.get(key);
+							for (String otherin : input) {
+								if (otherin.length() > Integer.parseInt(key)) {
+//									System.out.println("other:"+otherin + ":char to check:"+ _char + ":@pos:" + key);
+//									System.out.println("found char:" + otherin.charAt(Integer.parseInt(key)));
+//									System.out.println("bool check (!otherin.equals(in)):" + !otherin.equals(in));
+//									System.out.println("bool check (!inputUsed.contains(otherin)):" + !inputUsed.contains(otherin));
+//									System.out.println("bool check (_char.equals(otherin.charAt(Integer.parseInt(key)))):" + _char.equals(String.valueOf(otherin.charAt(Integer.parseInt(key)))) );
+									if (!otherin.equals(in) && !inputUsed.contains(otherin) && _char.equals(String.valueOf(otherin.charAt(Integer.parseInt(key))))) {
+//										System.out.println("are we here..");
+										inStrPositionContains = true;
+										positionalStatus[pos] = true;
+										break;
+									}
+								}
+							}
+							
+							if (!inStrPositionContains) {
+//								System.out.println("its a wrong string... lets go to the next one");
+								inStrPositionContains = false;
+							}
+							pos++;
+						}
+						
+						// check if a string matches all intersecional positions
+						boolean fillStatus = true;
+						int k = 0;
+						for (Boolean positionStatus : positionalStatus) {
+//							System.out.println("pos:" + k++ +":" + positionStatus.booleanValue());
+							fillStatus = fillStatus && positionStatus.booleanValue();
+						}
+						
+						if (fillStatus) {
+							toFill = in;
+							break;
+						}
+					} else {
+						toFill = in;
+						break;
+					}
 				}
 			}
 			
@@ -244,9 +260,125 @@ public class Crossword {
 			inputUsed.add(toFill);
 		}
 	}
-	
-	public static void placeWordsInColsMatrix(String matrix[][], String rowScan[], String colScan[], String interScan[], String input[], List inputUsed) {
 		
+	public static void placeWordsInColsMatrix(String matrix[][], String rowScan[], String colScan[], String interScan[], String input[], List inputUsed) {
+//		System.out.println("******* IN COL ********");
+		for (Object str: inputUsed) {
+//			System.out.println(str.toString());
+		}
+		for (String col : colScan) {
+			if (col == null || col.length() == 0)
+				continue;
+			
+			String colPos[] = col.split(";");
+			String startPosN = colPos[0];
+			String endPosN = colPos[colPos.length -1];
+			int startY = Integer.parseInt(startPosN.split(",")[1]);
+			int endY = Integer.parseInt(endPosN.split(",")[1]);
+			int startX = Integer.parseInt(startPosN.split(",")[0]);
+			int endX = Integer.parseInt(endPosN.split(",")[0]);
+			int len = (endX - startX) + 1;
+			
+//			System.out.println("col 2 fill:" + col + ":len:" + len);
+			
+			String toFill = "";
+			
+			for (String in : input) {
+//				System.out.println("in string:" + in);
+				if (in.length() == len && !inputUsed.contains(in)) {
+//					System.out.println("fillll:" + col);
+					Map<String,String> positionalIndexes = new HashMap<String, String>();
+					Map<String,String> interPositionIndex = new HashMap<String, String>();
+					
+					for (String inter : interScan) {
+						if (inter != null && col.indexOf(inter) != -1) {
+							// to get a map of intersected position and letters 
+							// e.g. 3,1; is D
+							String letter = String.valueOf(in.charAt(col.indexOf(inter)/4));
+							positionalIndexes.put(inter, letter);
+//							System.out.println("filling:" + inter + ":with:" + letter);
+							
+							// to get a map of position and letters 
+							// e.g. 3 is D
+							interPositionIndex.put(String.valueOf(col.indexOf(inter)/4), letter);
+//							System.out.println("inter filling:" + String.valueOf(col.indexOf(inter)/4) + ":with:" + letter);
+							
+						}
+					}
+					
+					
+					if (positionalIndexes.size() > 0) {
+						Iterator<String> itr = positionalIndexes.keySet().iterator();
+						while(itr.hasNext()) {
+							String key = itr.next();
+//							System.out.println(":position index:" + key + ":va:" + positionalIndexes.get(key));
+						}
+						
+						itr = interPositionIndex.keySet().iterator();
+						while(itr.hasNext()) {
+							String key = itr.next();
+//							System.out.println(":intersection position index:" + key + ":iiiva:" + interPositionIndex.get(key));
+						}
+						
+						itr = interPositionIndex.keySet().iterator();
+						boolean inStrPositionContains = false;
+						Boolean[] positionalStatus = new Boolean[interPositionIndex.keySet().size()];
+						Arrays.fill(positionalStatus, Boolean.FALSE);
+						int pos = 0;
+						while(itr.hasNext()) {
+							String key = itr.next();
+							String _char = interPositionIndex.get(key);
+							for (String otherin : input) {
+								if (otherin.length() >= Integer.parseInt(key)) {
+//									System.out.println("other:"+otherin + ":char 2 check:"+ _char + ":@pos:" + key);
+//									System.out.println("found char:" + otherin.charAt(Integer.parseInt(key)));
+//	//								System.out.println("bool check (!otherin.equals(in)):" + !otherin.equals(in));
+//									System.out.println("bool check (!inputUsed.contains(otherin)):" + !inputUsed.contains(otherin));
+//									System.out.println("bool check (_char.equals(otherin.charAt(Integer.parseInt(key)))):" + _char.equals(String.valueOf(otherin.charAt(Integer.parseInt(key)))) );
+									if (!inputUsed.contains(otherin) && _char.equals(String.valueOf(otherin.charAt(Integer.parseInt(key))))) {
+//										System.out.println("are we here..");
+										inStrPositionContains = true;
+										positionalStatus[pos] = true;
+										break;
+									}
+								}
+							}
+							
+							if (!inStrPositionContains) {
+//								System.out.println("its a wrong string... lets go to the next one");
+								inStrPositionContains = false;
+							}
+							pos++;
+						}
+						
+						// check if a string matches all intersecional positions
+						boolean fillStatus = true;
+						int k = 0;
+						for (Boolean positionStatus : positionalStatus) {
+//							System.out.println("pos:" + k++ +":" + positionStatus.booleanValue());
+							fillStatus = fillStatus && positionStatus.booleanValue();
+						}
+						
+						if (fillStatus) {
+							toFill = in;
+							break;
+						}
+					} else {
+						toFill = in;
+						break;
+					}
+				}
+			}
+			
+			if (toFill.equals("")) {
+				break;
+			}
+			int k = 0;
+			for (int j = startX; j<= endX; j++){
+				matrix[j][startY]=""+toFill.charAt(k++);
+			}
+			inputUsed.add(toFill);
+		}
 	}
 	
 
